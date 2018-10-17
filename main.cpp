6d574ced3524d1fb380182d95a040be17570bf59
Scott Harvey <scott@spharvey.me>
2018-10-17 07:14:52 -0500

    WIP: Adding QML engine

--- main.cpp
+++ main.cpp
@@ -1,42 +1,22 @@
-/*
- * Copyright 2018, Adriaan de Groot <groot@kde.org>
- * 
- * This file is part of YAMLAMLDINGDONG (hereafter YDD).
- * 
- *   YDD is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   YDD is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with YDD. If not, see <http://www.gnu.org/licenses/>.
- * 
- *   SPDX-License-Identifier: GPLv3+
- */
-
-/* main.cpp
- * 
- * Run the program with a single argument (a filename) to parse that YAML 
- * file and do stuff with it.
- */
-
-#include <iostream>
-
-#include <QCoreApplication>
-
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 #include "Job.h"
 #include "YamlUtils.h"
 
 #include "ChoiceGroup.h"
 
-int main(int argc, char **argv)
+#include <iostream>
+
+int main(int argc, char *argv[])
 {
-    QCoreApplication a( argc, argv );
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     if ( argc == 2 )
     {
@@ -64,5 +44,6 @@ int main(int argc, char **argv)
     std::cerr << "Usage: " << argv[0] << " <filename>\n\n"
         << "Parses YAML document <filename> and does stuff with it.\n";
         
-    return 1;
+    
+    return app.exec();
 }
